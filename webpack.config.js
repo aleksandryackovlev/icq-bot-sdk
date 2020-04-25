815c4cd080876053e8c1ad4807c93207beb693f1
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2020-04-25 18:11:02 +0300

    build(webpack): set up a webpack config

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const path = require('path');
+
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  mode: 'production',
+  devtool: false,
+  entry: path.resolve(__dirname, './src/index.js'),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
+  },
+  externals: [nodeExternals()],
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.js', '.mjs', '.yaml'],
+  },
+  optimization: {
+    minimize: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(yaml|js)$/i,
+        rules: [
+          {
+            loader: require.resolve('openapi-client-sdk-loader'),
+            options: {
+              compiler: 'js',
+              templateOptions: {
+                validateRequest: true,
+                validateResponse: true,
+              },
+              skipInvalid: true,
+              style: {
+                singleQuote: true,
+                trailingComma: 'es5',
+                printWidth: 100,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [],
+};
