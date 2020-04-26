ea850066df63540d1f3167e59259220fd163d52d
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2020-04-26 16:02:47 +0300

    feat(icq-client): add events handling to the client

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import EventEmitter from 'events';
+
 import * as icqApi from './api/api.yaml';
 
+/* eslint-disable no-await-in-loop */
 const ICQClient = ({ token, apiUrl = 'https://api.icq.net/bot/v1' } = {}) => {
-  return Object.keys(icqApi).reduce((acc, method) => {
+  let icqClient = Object.keys(icqApi).reduce((acc, method) => {
     const [prefix, methodName] = method.split('_');
 
     return methodName
@@ -33,6 +36,44 @@ const ICQClient = ({ token, apiUrl = 'https://api.icq.net/bot/v1' } = {}) => {
         }
       : acc;
   }, {});
+
+  icqClient = {
+    ...icqClient,
+    isRunning: false,
+    sleep: (timeout) =>
+      new Promise((resolve) => {
+        setTimeout(resolve, timeout);
+      }),
+    async startPolling({ pollTime = 1, timeout = 5, lastEventId = null } = {}) {
+      this.isRunning = true;
+
+      const eventId = lastEventId || 0;
+
+      while (this.isRunning) {
+        try {
+          const {
+            json: { events },
+          } = await this.events.get({ lastEventId: eventId, pollTime });
+
+          events.forEach(({ type, ...response }) => {
+            this.emit(type, response);
+            this.emit('all', response);
+          });
+        } catch (error) {
+          this.emit('error', error);
+        }
+
+        await this.sleep(timeout * 1000);
+      }
+    },
+    stop() {
+      this.isRunning = false;
+    },
+  };
+
+  Object.setPrototypeOf(icqClient, new EventEmitter());
+
+  return icqClient;
 };
 
 export default ICQClient;
